994e9f2300242278982055171aa3e1e59a3d63f1
AnnGolub <anka.golub17@gmail.com>
2021-03-28 14:47:29 +0300

    Initial Commit

---
+++ UiKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiKitApp
+//
+//  Created by Анна Голубева on 28.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
