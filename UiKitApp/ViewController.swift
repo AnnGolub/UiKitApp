4d73fa6f632d50d01d6b8f7e9ca6e2663fba616f
AnnGolub <anka.golub17@gmail.com>
2021-03-29 10:04:18 +0300

    3.1

--- UiKitApp/ViewController.swift
+++ UiKitApp/ViewController.swift
@@ -39,7 +39,10 @@ class ViewController: UIViewController {
         blueValue.text = String(format: "%.2f", blueSlider.value)
     }
     @IBAction func slidersAction() {
-        viewColour.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        viewColour.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                             green: CGFloat(greenSlider.value),
+                                             blue: CGFloat(blueSlider.value),
+                                             alpha: 1)
     }
 }
 
